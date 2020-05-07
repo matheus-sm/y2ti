b32b4b3d1b4f53e0aba5401967a247518a5667e0
Matheus Sommacal Melo <matheus.sommacal@hotmail.com>
2020-05-07 20:38:28 -0300

    first commit

---
+++ storage/logs/log-2020-05-07.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-05-07 03:01:08 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2020-05-07 03:01:08 --> #0 C:\xampp\htdocs\EA\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\EA\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\EA\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
+ERROR - 2020-05-07 03:20:33 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2020-05-07 03:20:33 --> #0 C:\xampp\htdocs\EA\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\EA\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\EA\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
+ERROR - 2020-05-07 05:24:44 --> Email could not been sent. Mailer Error (Line 179): Could not instantiate mail function.
+ERROR - 2020-05-07 05:24:44 --> #0 C:\xampp\htdocs\EA\application\controllers\Appointments.php(575): EA\Engine\Notifications\Email->sendAppointmentDetails(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text))
+#1 C:\xampp\htdocs\EA\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\EA\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
